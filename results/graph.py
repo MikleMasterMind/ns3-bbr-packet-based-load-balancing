ebcc81e50b00a88439f387d4df8e6270707b3a29
THEG <thegostoftower@gmail.com>
2025-11-26 00:10:44 +0300

    +graph

---
+++ results/graph.py
@@ -0,0 +1,27 @@
+# result/plot_results.py
+import matplotlib.pyplot as plt
+import numpy as np
+
+# Чтение данных
+cwnd_data = np.loadtxt('results/data/cwnd.data', skiprows=1)
+
+# Создание графиков
+fig, ((ax1, ax4)) = plt.subplots(2, figsize=(12, 8))
+
+# График окна перегрузки
+ax1.plot(cwnd_data[:, 0], cwnd_data[:, 1])
+ax1.set_xlabel('Время (с)')
+ax1.set_ylabel('Размер окна (сегменты)')
+ax1.set_title('Окно перегрузки TCP Cubic')
+ax1.grid(True)
+
+# Гистограмма распределения размера окна
+ax4.hist(cwnd_data[:, 1], bins=50, alpha=0.7)
+ax4.set_xlabel('Размер окна (сегменты)')
+ax4.set_ylabel('Частота')
+ax4.set_title('Распределение размера окна')
+ax4.grid(True)
+
+plt.tight_layout()
+plt.savefig('results/graph/tcp_cubic_analysis.png', dpi=300)
+plt.show()
